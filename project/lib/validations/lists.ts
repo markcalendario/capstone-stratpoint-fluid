41c4abff642637dfce6cd379b6d118540c6caf0d
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-24 11:27:28 +0800

    feat: add zod schema validation for lists

---
+++ project/lib/validations/lists.ts
@@ -0,0 +1,26 @@
+import z from "zod";
+
+const MAX_NAME = 10;
+
+export const createListSchema = z.object({
+  name: z
+    .string()
+    .max(MAX_NAME, `Max list name length is ${MAX_NAME} characters.`),
+  projectId: z.uuidv4("Project ID must be a UUID."),
+  position: z.number("Position must be a number.").positive("Invalid position.")
+});
+
+export const updateListSchema = z.object({
+  name: z
+    .string()
+    .max(MAX_NAME, `Max list name length is ${MAX_NAME} characters.`),
+  projectId: z.uuidv4("Project ID must be a UUID."),
+  position: z
+    .number("Position must be a number.")
+    .positive("Invalid position."),
+  updatedAt: z.iso.datetime("Invalid date for date modified.")
+});
+
+export const getListByIdSchema = z.uuidv4("List ID is an invalid UUID.");
+
+export const deleteListSchema = z.uuidv4("List ID is an invalid UUID.");
