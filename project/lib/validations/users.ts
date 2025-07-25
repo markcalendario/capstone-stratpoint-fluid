8f7e6cf196a08249d971ada2b4d9789cd0d5fa23
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-25 13:09:38 +0800

    fix: change clerkId uuid type to string

--- project/lib/validations/users.ts
+++ project/lib/validations/users.ts
@@ -2,13 +2,13 @@ import z from "zod";
 
 export const createUserSchema = z.object({
   name: z.string("Name must be a string.").trim(),
-  clerkId: z.uuidv4("Clerk ID is invalid."),
+  clerkId: z.string("Clerk ID must be string."),
   email: z.email("Invalid email address.").trim()
 });
 
 export const updateUserSchema = z.object({
   name: z.string("Name must be a string.").trim(),
-  clerkId: z.uuidv4("Clerk ID is invalid."),
+  clerkId: z.string("Clerk ID is invalid."),
   email: z.email("Invalid email address.").trim(),
   updatedAt: z.iso.datetime("Invalid update date and time.")
 });
