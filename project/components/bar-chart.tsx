d5a5e87106f200c91b53e19bcfc89e74a159a660
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-30 15:55:30 +0800

    style: revamp bar-chart style

--- project/components/bar-chart.tsx
+++ project/components/bar-chart.tsx
@@ -2,6 +2,7 @@
 
 import {
   Bar,
+  CartesianGrid,
   BarChart as Chart,
   Legend,
   ResponsiveContainer,
@@ -19,8 +20,8 @@ export default function BarChart({ data }: { data: any }) {
         data={data}
         width={730}
         height={250}
-        barCategoryGap="20%"
         barGap={4}>
+        <CartesianGrid strokeDasharray="3 3" />
         <XAxis
           type="category"
           dataKey="name"
@@ -34,14 +35,18 @@ export default function BarChart({ data }: { data: any }) {
         <Bar
           dataKey="Done"
           stackId="a"
-          fill="#0d542b"
+          fill="#00C49F"
           unit="%"
+          opacity="70%"
+          stroke="#00C49F"
         />
         <Bar
           dataKey="Pending"
           stackId="a"
-          fill="#e37837"
+          fill="#ffc658"
           unit="%"
+          opacity="70%"
+          stroke="#ffc658"
         />
       </Chart>
     </ResponsiveContainer>
