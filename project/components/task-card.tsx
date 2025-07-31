5678ba49bde0cc51a9e91abfda713196b49dbd96
mkcalendario <i_mark.calendario@stratpoint.com>
2025-07-31 11:40:51 +0800

    feat: add task card component

--- project/components/task-card.tsx
+++ project/components/task-card.tsx
@@ -1,5 +1,9 @@
 // TODO: Task 5.6 - Create task detail modals and editing interfaces
 
+import { TaskCard as TTaskCard } from "@/types/tasks";
+import Image from "next/image";
+import PriorityTab from "./priority-tab";
+
 /*
 TODO: Implementation Notes for Interns:
 
@@ -37,12 +41,32 @@ Features to implement:
 - Responsive design
 */
 
-export function TaskCard() {
+export function TaskCard({
+  id,
+  title,
+  description,
+  priority,
+  assigneeName,
+  assigneeImageUrl
+}: TTaskCard) {
   return (
-    <div className="rounded-lg border border-french_gray-300 bg-white p-4 dark:border-payne's_gray-400 dark:bg-outer_space-300">
-      <p className="text-center text-sm text-payne's_gray-500 dark:text-french_gray-400">
-        TODO: Implement TaskCard component
+    <div className="outline-primary/20 cursor-pointer rounded-xs bg-white p-4 shadow-sm outline-2 hover:shadow-md dark:bg-neutral-600">
+      <h4 className="mb-1 text-sm font-medium text-neutral-800 dark:text-neutral-100">
+        {title}
+      </h4>
+      <p className="mb-2 text-xs text-neutral-500 dark:text-neutral-300">
+        {description}
       </p>
+      <div className="flex items-center justify-between">
+        <PriorityTab priority={priority} />
+        <Image
+          width={25}
+          height={25}
+          alt={assigneeName}
+          src={assigneeImageUrl}
+          className="outline-primary/20 rounded-full outline-2"
+        />
+      </div>
     </div>
   );
 }
