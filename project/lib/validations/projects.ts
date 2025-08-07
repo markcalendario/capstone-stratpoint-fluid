dba037caf2827b0932c86d1a2a3202aa9f2e919f
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-08-07 10:57:28 +0800

    refactor: create general projects schema and shape the query validations

--- project/lib/validations/projects.ts
+++ project/lib/validations/projects.ts
@@ -1,35 +1,13 @@
 import { isFutureDate } from "@/lib/utils/date-and-time";
 import z from "zod";
+import { userSchema } from "./users";
 
 const MIN_NAME = 3;
 const MIN_DESCRIPTION = 5;
 const MAX_NAME = 20;
 const MAX_DESCRIPTION = 50;
 
-export const createProjectSchema = z.object({
-  name: z
-    .string("Project name must be string.")
-    .trim()
-    .min(MIN_NAME, `Project name must be at least ${MIN_NAME} characters.`)
-    .max(MAX_NAME, `Max project name length is ${MAX_NAME} characters.`),
-  description: z
-    .string("Description must be a string.")
-    .trim()
-    .min(
-      MIN_DESCRIPTION,
-      `Description must be at least ${MIN_DESCRIPTION} characters.`
-    )
-    .max(
-      MAX_DESCRIPTION,
-      `Max description length is ${MAX_DESCRIPTION} characters.`
-    ),
-  dueDate: z.iso
-    .date("Due date must be in YYYY-MM-DD format.")
-    .refine(isFutureDate, "Due date cannot be in the past."),
-  ownerId: z.uuidv4("Owner ID must be a UUID.")
-});
-
-export const updateProjectSchema = z.object({
+export const projectSchema = z.object({
   id: z.uuidv4("Project ID must be UUID.").trim(),
   name: z
     .string("Project name must be string.")
@@ -47,11 +25,29 @@ export const updateProjectSchema = z.object({
       MAX_DESCRIPTION,
       `Max description length is ${MAX_DESCRIPTION} characters.`
     ),
+  ownerId: userSchema.shape.id,
+  createdAt: z.iso.datetime("Invalid date for date created.").trim(),
+  updatedAt: z.iso.datetime("Invalid date for date modified.").trim(),
   dueDate: z.iso
     .date("Due date must be in YYYY-MM-DD format.")
-    .refine(isFutureDate, "Due date cannot be in the past."),
-  ownerId: z.uuidv4("Owner ID must be a UUID."),
-  updatedAt: z.iso.datetime("Invalid date for date modified.")
+    .trim()
+    .refine(isFutureDate, "Due date cannot be in the past.")
 });
 
-export const projectIdSchema = z.uuidv4("Project ID is an invalid UUID.");
+export const createProjectSchema = z.object({
+  name: projectSchema.shape.name,
+  description: projectSchema.shape.description,
+  dueDate: projectSchema.shape.dueDate,
+  ownerId: projectSchema.shape.ownerId
+});
+
+export const updateProjectSchema = z.object({
+  id: projectSchema.shape.id,
+  name: projectSchema.shape.name,
+  description: projectSchema.shape.description,
+  dueDate: projectSchema.shape.dueDate,
+  ownerId: projectSchema.shape.ownerId,
+  updatedAt: projectSchema.shape.updatedAt
+});
+
+export const projectIdSchema = projectSchema.shape.id;
