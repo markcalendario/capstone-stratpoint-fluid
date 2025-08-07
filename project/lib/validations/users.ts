bac6c8fad7e5bcd2fe949349c78479bb9156e767
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-08-07 10:49:11 +0800

    refactor: create general user schema and shape the query validations

--- project/lib/validations/users.ts
+++ project/lib/validations/users.ts
@@ -1,15 +1,30 @@
 import z from "zod";
 
-export const createUserSchema = z.object({
-  name: z.string("Name must be a string.").trim(),
+export const userSchema = z.object({
+  id: z.uuid("User ID must be a UUID.").trim(),
+  clerkId: z.string("Clerk ID must be a string."),
   email: z.email("Invalid email address.").trim(),
-  clerkId: z.string("Clerk ID must be string.").trim()
+  name: z.string("Name must be a string.").trim(),
+  createdAt: z.iso
+    .datetime("Value for 'createdAt' field must be datetime.")
+    .trim(),
+  updatedAt: z.iso
+    .datetime("Value for 'updatedAt' field must be datetime.")
+    .trim()
+});
+
+// Query Validations
+
+export const createUserSchema = z.object({
+  name: userSchema.shape.name,
+  email: userSchema.shape.email,
+  clerkId: userSchema.shape.clerkId
 });
 
 export const updateUserSchema = z.object({
-  name: z.string("Name must be a string.").trim(),
-  email: z.email("Invalid email address.").trim(),
-  updatedAt: z.iso.datetime("Invalid update date and time.")
+  name: userSchema.shape.name,
+  email: userSchema.shape.email,
+  updatedAt: userSchema.shape.updatedAt
 });
 
 export const userIdSchema = z.uuidv4("User ID is an invalid UUID.");
