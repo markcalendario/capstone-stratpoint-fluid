df13a788d162b947c6d30ddcfd57b5af074ae39a
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-08-07 11:02:29 +0800

    refactor: create general list schema and shape the query validations

--- project/lib/validations/lists.ts
+++ project/lib/validations/lists.ts
@@ -1,27 +1,35 @@
 import z from "zod";
+import { projectSchema } from "./projects";
+import { userSchema } from "./users";
 
-const MAX_NAME = 20;
 const MIN_NAME = 3;
+const MAX_NAME = 20;
 
-export const createListSchema = z.object({
+export const listSchema = z.object({
+  id: z.uuidv4("List ID is an invalid UUID.").trim(),
   name: z
     .string("List name must be string.")
     .trim()
     .min(MIN_NAME, `Minimum list name length is ${MIN_NAME} characters.`)
     .max(MAX_NAME, `Maximum list name length is ${MAX_NAME} characters.`),
-  projectId: z.uuidv4("Project ID must be a UUID."),
-  createdBy: z.uuidv4("List creator ID must be UUID."),
+  projectId: projectSchema.shape.id,
+  createdBy: userSchema.shape.id,
+  updatedAt: z.iso.datetime("Invalid date for date modified.").trim(),
+  createdAt: z.iso.datetime("Invalid date for date created.").trim(),
   isFinal: z.boolean("isFinal field must be boolean.")
+});
+
+export const createListSchema = z.object({
+  name: listSchema.shape.name,
+  projectId: listSchema.shape.projectId,
+  createdBy: listSchema.shape.createdBy,
+  isFinal: listSchema.shape.isFinal
 });
 
 export const updateListSchema = z.object({
-  name: z
-    .string("List name must be string.")
-    .trim()
-    .min(MIN_NAME, `Minimum list name length is ${MIN_NAME} characters.`)
-    .max(MAX_NAME, `Maximum list name length is ${MAX_NAME} characters.`),
-  updatedAt: z.iso.datetime("Invalid date for date modified."),
-  isFinal: z.boolean("isFinal field must be boolean.")
+  name: listSchema.shape.name,
+  updatedAt: listSchema.shape.updatedAt,
+  isFinal: listSchema.shape.isFinal
 });
 
-export const listIdSchema = z.uuidv4("List ID is an invalid UUID.");
+export const listIdSchema = listSchema.shape.id;
