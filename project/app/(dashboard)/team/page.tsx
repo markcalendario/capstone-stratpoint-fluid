0c7c904f2dadd3ded110ab8d9ec9ed42fb73378f
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-08-25 08:52:24 +0800

    fix: build errors

--- project/app/(dashboard)/team/page.tsx
+++ project/app/(dashboard)/team/page.tsx
@@ -1,5 +1,7 @@
 import { DashboardContent } from "@/components/layouts/dashboard/dashboard-content";
 import TeamMembersListGrid from "@/components/sections/team/team-grid-list";
+import SectionLoader from "@/components/ui/section-loader";
+import { Suspense } from "react";
 
 export default function TeamPage() {
   return (
@@ -24,7 +26,9 @@ export default function TeamPage() {
         </div>
 
         {/* Team Members Grid */}
-        <TeamMembersListGrid />
+        <Suspense fallback={<SectionLoader text="Loading Members" />}>
+          <TeamMembersListGrid />
+        </Suspense>
       </div>
     </DashboardContent>
   );
