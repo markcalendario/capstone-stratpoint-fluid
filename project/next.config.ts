17312458d05a01b00d78eebe149bbe227353b288
mkcalendario <i_mark.calendario@stratpoint.com>
2025-08-29 17:36:41 +0800

    refactor: bring strict mode back

--- project/next.config.ts
+++ project/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false,
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "img.clerk.com" },
